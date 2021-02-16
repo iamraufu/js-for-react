e7fcce1f66de09c1f37cf74cb3f804bb41820cc4
Eftykhar Rahman <eftykharraufu@gmail.com>
2021-02-17 00:10:02 +0600

    1

---
+++ 1-core-concepts.js
@@ -0,0 +1,56 @@
+//  variable : let, const 
+let money = 50;
+money = 10;
+
+const name = "Kudos"
+
+let isTasty = true
+
+const person = {
+    name: 'kudos',
+    age: 60,
+    city: 'Dhaka'
+}
+
+const statement = `this person ${name} with age ${person.age} live near ${person.city}`
+
+// if else
+if (money > 20 && age >= 5) {
+
+} else {
+
+}
+
+// array
+const numbers = [10, 20, 60, 40]
+const respected = ['motiur', 'sattar', 'jobbar']
+const products = [{
+        name: 'laptop',
+        price: 100
+    },
+    {
+        name: 'mobile',
+        price: 20
+    }
+
+]
+
+// loop: for loop
+for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+
+}
+
+// function: regular arrow
+function add(n1, n2) {
+    return n1 + n2
+}
+const result = add(2, 3)
+
+// arrow
+const add2 = (n1, n2) => n1 + n2
+const fiveTimes = n1 => n1 * 5
+const doMath = (x, y) => {
+    const sum = x + y
+    return sum
+}
